423ea63c6009809eca77917f2b8d9b8d25bcda52
Ali Hyder <alihyder23@gmail.com>
2016-01-01 15:57:13 -0800

    starting FoodTracker App

---
+++ FoodTracker/FoodTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Ali Hyder on 1/1/16.
+//  Copyright © 2016 Ali Hyder. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
